a07de2d5d3ac68c9958554660fa69c5d844733b0
hache0099 <hache0099_github@protonmail.com>
2023-12-15 16:36:07 -0300

    Now the functions only runs if the link has "youtube.com" in its URL

--- background.js
+++ background.js
@@ -9,15 +9,21 @@ browser.runtime.onInstalled.addListener(() => {
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "plaympv"){
+    if (!info.linkUrl.includes("youtube.com")){
+      return
+    }
+    console.log(info.linkUrl);
     let xhr = new XMLHttpRequest();
 
+    xhr.open("GET","http://localhost:8090/play?url="+info.linkUrl)
     xhr.onreadystatechange = () => {
       if (xhr.readyState !== XMLHttpRequest.DONE){
         return
       }
-      console.log(xhr.responseText)
+      console.log("xhr response: ",xhr.responseText)
     }
     
+    xhr.send();
   }
 });
 
